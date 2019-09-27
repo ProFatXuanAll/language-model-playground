3cf20526b29a8763de65f43a4dd11d418973d135
ProFatXuanAll <ProFatXuanAll@gmail.com>
2019-09-27 18:40:01 +0800

    fix config

--- lmp/config.py
+++ lmp/config.py
@@ -17,16 +17,7 @@ class BaseConfig:
 
     @classmethod
     def load_from_file(cls, file_path=None, **kwargs):
-        cls.batch_size = kwargs.pop('batch_size', 1)
-        cls.dropout = kwargs.pop('dropout', 0)
-        cls.embedding_dim = kwargs.pop('embedding_dim', 1)
-        cls.epoch = kwargs.pop('epoch', 1)
-        cls.grad_clip_value = kwargs.pop('grad_clip_value', 1)
-        cls.hidden_dim = kwargs.pop('hidden_dim', 1)
-        cls.learning_rate = kwargs.pop('learning_rate', 10e-4)
-        cls.min_count = kwargs.pop('min_count', 0)
-        cls.num_rnn_layers = kwargs.pop('num_rnn_layers', 1)
-        cls.seed = kwargs.pop('seed', 1)
+        self = cls()
 
         if file_path is None or type(file_path) != str:
             raise ValueError('argument `file_path` should be a string')
@@ -35,18 +26,18 @@ class BaseConfig:
 
         with open(file_path, 'rb') as f:
             hyperparameters = pickle.load(f)
-            cls.batch_size = hyperparameters.pop('batch_size', cls.batch_size)
-            cls.dropout = hyperparameters.pop('dropout', cls.dropout)
-            cls.embedding_dim = hyperparameters.pop('embedding_dim', cls.embedding_dim)
-            cls.epoch = hyperparameters.pop('epoch', cls.epoch)
-            cls.grad_clip_value = hyperparameters.pop('grad_clip_value', cls.grad_clip_value)
-            cls.hidden_dim = hyperparameters.pop('hidden_dim', cls.hidden_dim)
-            cls.learning_rate = hyperparameters.pop('learning_rate', cls.learning_rate)
-            cls.min_count = hyperparameters.pop('min_count', cls.min_count)
-            cls.num_rnn_layers = hyperparameters.pop('num_rnn_layers', cls.num_rnn_layers)
-            cls.seed = hyperparameters.pop('seed', cls.seed)
+            self.batch_size = hyperparameters.pop('batch_size', self.batch_size)
+            self.dropout = hyperparameters.pop('dropout', self.dropout)
+            self.embedding_dim = hyperparameters.pop('embedding_dim', self.embedding_dim)
+            self.epoch = hyperparameters.pop('epoch', self.epoch)
+            self.grad_clip_value = hyperparameters.pop('grad_clip_value', self.grad_clip_value)
+            self.hidden_dim = hyperparameters.pop('hidden_dim', self.hidden_dim)
+            self.learning_rate = hyperparameters.pop('learning_rate', self.learning_rate)
+            self.min_count = hyperparameters.pop('min_count', self.min_count)
+            self.num_rnn_layers = hyperparameters.pop('num_rnn_layers', self.num_rnn_layers)
+            self.seed = hyperparameters.pop('seed', self.seed)
 
-        return cls
+        return self
 
     def save_to_file(self, file_path=None):
         if file_path is None or type(file_path) != str:
