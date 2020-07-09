6046f315a103a5a5fc0dc5c8b483cf8c2d6961ed
unknown <allen521105@gmail.com>
2020-07-09 17:26:10 +0800

    token_to_id being list, type annotations

--- lmp/model.py
+++ lmp/model.py
@@ -11,19 +11,24 @@ import torch.nn.utils.rnn
 import torch.optim
 import sklearn.metrics
 from tqdm import tqdm
-
+from typing import List, Union
+import lmp.tokenizer
+import lmp.config
 ##############################################
 # build model
 ##############################################
 
-class BaseModel(torch.nn.Module):
-    def __init__(self, config, tokenizer):
-        super(BaseModel, self).__init__()
 
+class BaseModel(torch.nn.Module):
+    def __init__(self, config: lmp.config.BaseConfig,
+                 tokenizer: Union[lmp.tokenizer.BaseTokenizer, lmp.tokenizer.BaseTokenizerDict]):
+        super(BaseModel, self).__init__()
+        
         # Embedding layer
         self.embedding_layer = torch.nn.Embedding(num_embeddings=tokenizer.vocab_size(),
                                                   embedding_dim=config.embedding_dim,
                                                   padding_idx=tokenizer.pad_token_id)
+        
 
         # RNN layer
         self.rnn_layer = torch.nn.RNN(input_size=config.embedding_dim,
@@ -31,53 +36,63 @@ class BaseModel(torch.nn.Module):
                                       num_layers=config.num_rnn_layers,
                                       dropout=config.dropout,
                                       batch_first=True)
-
+        
         # Forward passing
         # self.linear = torch.nn.Linear(config.hidden_dim, config.embedding_dim)
         self.linear = []
 
         for _ in range(config.num_linear_layers):
-            self.linear.append(torch.nn.Linear(config.hidden_dim, config.hidden_dim))
+            self.linear.append(torch.nn.Linear(
+                config.hidden_dim, config.hidden_dim))
             self.linear.append(torch.nn.ReLU())
             self.linear.append(torch.nn.Dropout(config.dropout))
 
-        self.linear.append(torch.nn.Linear(config.hidden_dim, config.embedding_dim))
+        self.linear.append(torch.nn.Linear(
+            config.hidden_dim, config.embedding_dim))
+        
         self.sequential = torch.nn.Sequential(*self.linear)
 
-    def forward(self, batch_x):
+    def forward(self, batch_x: torch.Tensor) -> torch.Tensor:
         ######################################################################
-        # 維度: (batch_Size, sequence_length)
-        #
-        # 維度: (batch_size, sequence_length, embedding_dimension)
+        # embedding前 的 batch_x 維度: (batch_Size, sequence_length)
+        # 
+        # embedding後 的 batch_x維度: (batch_size, sequence_length, embedding_dimension)
         ######################################################################
         batch_x = self.embedding_layer(batch_x)
-
+  
         ######################################################################
-        # 維度: (batch_size, sequence_length, hidden_dimension)
+        # ht 維度: (batch_size, sequence_length, hidden_dimension)
         ######################################################################
         ht, _ = self.rnn_layer(batch_x)
 
+        
         ######################################################################
-        # 維度: (batch_size, sequence_length, vocabulary_size)
+        # ht 維度: (batch_size, sequence_length, embedding_dimension)
         ######################################################################
         #ht = self.linear(ht)
         ht = self.sequential(ht)
+
+        ######################################################################
+        # yt 維度: (batch_size, sequence_length, vocabulary_size)
+        ######################################################################
         yt = ht.matmul(self.embedding_layer.weight.transpose(0, 1))
+        
         return yt
 
     def generator(self,
-                  tokenizer=None,
-                  begin_of_sentence='',
-                  beam_width=4,
-                  max_len=200):
+                  tokenizer: Union[lmp.tokenizer.BaseTokenizer, lmp.tokenizer.BaseTokenizerDict],
+                  begin_of_sentence: str = '',
+                  beam_width: int = 4,
+                  max_len: int = 200) -> List[str]:
         if begin_of_sentence is None or len(begin_of_sentence) == 0:
             raise ValueError('`begin_of_sentence` should be list type object.')
 
-        generate_result = []
+        generate_result: List[List[int]] = []
 
         with torch.no_grad():
             all_ids = tokenizer.convert_sentences_to_ids([begin_of_sentence])
             all_ids_prob = [0]
+            
 
             while True:
                 active_ids = []
@@ -96,23 +111,26 @@ class BaseModel(torch.nn.Module):
                     break
 
                 batch_x = [torch.LongTensor(ids) for ids in active_ids]
+                
                 batch_x = torch.nn.utils.rnn.pad_sequence(batch_x,
                                                           batch_first=True,
                                                           padding_value=tokenizer.pad_token_id)
-
                 batch_pred_y = self(batch_x)
 
+                
                 # 從各個句子中的 beam 挑出前 beam_width 個最大值
                 top_value = {}
                 for beam_id in range(len(active_ids)):
-                    current_beam_vocab_pred = batch_pred_y[beam_id][len(active_ids[beam_id]) - 1]
-                    current_beam_vocab_pred = torch.nn.functional.softmax(current_beam_vocab_pred, dim=0)
-
+                    current_beam_vocab_pred = batch_pred_y[beam_id][len(
+                        active_ids[beam_id]) - 1]   # current_beam_vocab_pred.shape() = ([vocab_size])
+                    current_beam_vocab_pred = torch.nn.functional.softmax(
+                        current_beam_vocab_pred, dim=0) 
                     current_beam_top_value = [{
                         'vocab_id': tokenizer.eos_token_id,
                         'value': 0
                     } for i in range(beam_width)]
 
+                    
                     for vocab_id in range(tokenizer.vocab_size()):
                         if current_beam_vocab_pred[vocab_id] < current_beam_top_value[0]['value']:
                             continue
@@ -129,7 +147,7 @@ class BaseModel(torch.nn.Module):
                                                          'value': current_beam_vocab_pred[vocab_id]}
 
                     top_value[beam_id] = current_beam_top_value
-
+                
                 # 從 beam_width ** 2 中挑出 beam_width 個最大值
                 final_top_value = []
 
@@ -139,10 +157,12 @@ class BaseModel(torch.nn.Module):
                     min_value = 999999999
 
                     for beam_id in range(len(top_value)):
-                        value = -torch.log(top_value[beam_id][-1]['value']) + active_ids_prob[beam_id]
+                        value = - \
+                            torch.log(
+                                top_value[beam_id][-1]['value']) + active_ids_prob[beam_id]
                         if value < min_value:
                             max_value_beam_id = beam_id
-                            min_value = value
+                            min_value = value.item()   # 從 tensor 變為 int，因為min_value是int(不轉的話一樣可運行，用mypy檢查會報error)
                             max_value_vocab_id = top_value[beam_id][-1]['vocab_id']
 
                     final_top_value.append({
@@ -157,13 +177,15 @@ class BaseModel(torch.nn.Module):
                 all_ids = []
                 all_ids_prob = []
                 for obj in final_top_value:
-                    all_ids.append(active_ids[obj['beam_id']] + [obj['vocab_id']])
+                    all_ids.append(
+                        active_ids[obj['beam_id']] + [obj['vocab_id']])
                     all_ids_prob.append(obj['value'])
 
         for ids in generate_result:
             if ids[-1] == tokenizer.eos_token_id:
                 ids.pop()
         return tokenizer.convert_ids_to_sentences(generate_result)
+
 
 class LSTMModel(BaseModel):
     def __init__(self, config, tokenizer):
@@ -175,6 +197,7 @@ class LSTMModel(BaseModel):
                                        num_layers=config.num_rnn_layers,
                                        dropout=config.dropout,
                                        batch_first=True)
+
 
 class GRUModel(BaseModel):
     def __init__(self, config, tokenizer):
