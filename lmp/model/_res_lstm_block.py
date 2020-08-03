833b0f7929cc6bc446eae089ba130cfa81c0b11f
unknown <allen521105@gmail.com>
2020-08-03 11:55:37 +0800

    add residul connection block and model

---
+++ lmp/model/_res_lstm_block.py
@@ -0,0 +1,55 @@
+r"""Residual LSTM block.
+
+Usage:
+    block = lmp.model.ResLSTMBlock(...)
+    logits = block(...)
+"""
+
+# built-in modules
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# 3rd-party modules
+
+import torch
+import torch.nn
+
+# self-made modules
+
+from lmp.model._base_res_rnn_block import BaseResRNNBlock
+
+class ResLSTMBlock(BaseResRNNBlock):
+    r"""Residual block with LSTM layers.
+
+    Args:
+        d_in:
+            Input data's vector dimension.
+        d_out:
+            GRU layers hidden dimension.
+        dropout:
+            Dropout probability on all layers out (except output layer).
+    """
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        dropout: float
+    ):
+        super().__init__(
+            d_in=d_in,
+            d_out=d_out,
+            dropout=dropout
+        )
+
+        # Override RNN layer with LSTM layer.
+        self.rnn_layer = torch.nn.LSTM(
+            input_size=d_in,
+            hidden_size=d_out,
+            num_layers=1,
+            dropout=dropout,
+            batch_first=True
+        )
+
---
+++ lmp/model/_residual_lstm_model.py
@@ -0,0 +1,103 @@
+r"""Language model with residual LSTM blocks.
+
+Usage:
+    from torch.utils.data import DataLoader
+    import lmp
+
+    model = lmp.model.ResidualLSTMModel(...)
+    tokenizer = lmp.tokenizer.CharDictTokenizer(...)
+    dataset = lmp.dataset.BaseDataset(...)
+    dataloader = DataLoader(
+        dataset=dataset,
+        collate_fn=dataset.create_collate_fn(tokenizer)
+    )
+    for x, y in dataloader:
+        pred = model(x)
+"""
+
+# built-in modules
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+# 3rd-party modules
+
+import torch
+
+# self-made modules
+
+import lmp.model
+
+from lmp.model._base_residual_rnn_model import BaseResidualRNNModel
+
+
+class ResidualLSTMModel(BaseResidualRNNModel):
+    r"""Language model with residual LSTM blocks.
+
+    Each input token will first be embedded into vectors, then sequentially
+    feed into residual LSTM blocks. Output vectors of blocks then go through
+    fully-connected layer and project back to embedding dimension in order to
+    perform vocabulary prediction.
+
+    In the comment below, we use following symbols to denote the size of
+    each tensors:
+        B: batch size
+        S: sequence length
+        E: embedding dimension
+        V: vocabulary size
+        H: hidden dimension
+
+    Args:
+        d_emb:
+            Embedding matrix vector dimension.
+        d_hid:
+            RNN layers hidden dimension.
+        dropout:
+            Dropout probability on all layers out (except output layer).
+        num_rnn_layers:
+            Number of RNN layers to use.
+        num_linear_layers:
+            Number of Linear layers to use.
+        pad_token_id:
+            Padding token's id. Embedding layers will initialize padding
+            token's vector with zeros.
+        vocab_size:
+            Embedding matrix vocabulary dimension.
+    """
+
+    def __init__(
+            self,
+            d_emb: int,
+            d_hid: int,
+            dropout: float,
+            num_rnn_layers: int,
+            num_linear_layers: int,
+            pad_token_id: int,
+            vocab_size: int
+    ):
+        super().__init__(
+            d_emb=d_emb,
+            d_hid=d_hid,
+            dropout=dropout,
+            num_rnn_layers=num_rnn_layers,
+            num_linear_layers=num_linear_layers,
+            pad_token_id=pad_token_id,
+            vocab_size=vocab_size
+        )
+
+        # Sequential residual LSTM blocks
+        # Dimension: (E, E)
+        lstm_blocks = []
+        for _ in range(num_rnn_layers):
+            lstm_blocks.append(
+                lmp.model.ResLSTMBlock(
+                    d_in=d_hid,
+                    d_out=d_hid,
+                    dropout=dropout
+                )
+            )
+
+        self.rnn_blocks = torch.nn.Sequential(*lstm_blocks)
