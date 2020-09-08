remove redundent code, fix consine similarity, add normalization to preprocessing

lmp/dataset/_language_model_dataset.py
@@ -34,7 +34,7 @@ CollateFn = Callable[[Iterable[str]], CollateFnReturn]
 
 
 class LanguageModelDataset(torch.utils.data.Dataset):
-    r"""Dataset class for generating language model samples.
+    r"""Dataset class generating language model samples.
 
     Attributes:
         batch_sequences: